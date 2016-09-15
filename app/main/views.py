671217d1b6c50e8fc7dc4a0bde3e35de5c0c766c
mapan1984 <mapan1984@outlook.com>
2016-09-15 11:42:23 +0800

    articles linking and showing have been done

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,18 @@
+import os
+
+
 from flask import render_template
 from . import main
 
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+targetdir = "\\".join(basedir.split('\\')[0:-1] + ['templates', 'post'])
+
 @main.route('/')
 def index():
-    return render_template('index.html')
+    post_list = os.listdir(targetdir)
+    return render_template('index.html', post_list=post_list)
+
+@main.route('/templates/post/<page_name>')
+def page(page_name):
+    return render_template('post/'+page_name)
--- generate.py
+++ generate.py
@@ -4,16 +4,36 @@
 """
 
 import os
+
 import markdown
-import markdown.extensions.codehilite
+from jinja2 import Environment, FileSystemLoader
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 sourcedir = basedir + '\\post\\'
 targetdir = basedir + '\\app\\templates\\post\\'
+layoutdir= basedir + '\\app\\templates\\_layouts'
 
-md = markdown.Markdown(extensions=["codehilite"])
+
+# markdown
+md = markdown.Markdown(
+    extensions=[
+        "markdown.extensions.codehilite(css_class=highlight,linenums=None)",
+        "markdown.extensions.meta",
+        "markdown.extensions.tables",
+        "markdown.extensions.toc",
+    ]
+)
+
+# jinja
+env = Environment(loader=FileSystemLoader(layoutdir))
+template = env.get_template("post.html")
 
 for post in os.listdir(sourcedir):
+    # 文章名
     post_name = post.split('.')[0]
-    md.convertFile(input=sourcedir+post, 
-                   output=targetdir+post_name+'.html')
+
+    with open(sourcedir+post, "r") as p:
+        content_html = md.convert(p.read())
+        html = template.render(post_content=content_html)
+    with open(targetdir+post_name+'.html', "w") as p:
+        p.write(html)
