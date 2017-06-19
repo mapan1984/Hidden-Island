5c03a153d8de035d00f025b4d61d65c1204c38d4
mapan1984 <mapan1984@outlook.com>
2017-06-19 20:43:47 +0800

    fix 403 error handle

--- app/main/errors.py
+++ app/main/errors.py
@@ -4,8 +4,12 @@ from . import main
 
 @main.app_errorhandler(404)
 def page_not_found(error):
-    return render_template('404.html'), 404
+    return render_template('errors/404.html'), 404
+
+@main.app_errorhandler(403)
+def page_not_found(error):
+    return render_template('errors/403.html'), 403
 
 @main.app_errorhandler(500)
 def internal_server_error(error):
-    return render_template('500.html'), 500
+    return render_template('errors/500.html'), 500
